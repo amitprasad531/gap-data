566595ec54a76edd89dccaf7131bc7b21346e7ca
Amit Prasad <amitprasad531@gmail.com>
2019-08-29 03:12:07 +0200

    Using PickerInput now and added latest available data function

--- global.R
+++ global.R
@@ -2,6 +2,7 @@ library(shiny)
 library(dplyr)
 library(DT)
 library(markdown)
+library(shinyWidgets)
 
 gap <- read.csv("data/gap.csv", header = TRUE)
 source("www/gapvectors.R")
@@ -9,4 +10,15 @@ source("www/metadata_data.R")
 
 for (i in 1:length(gap_ind)) {
   names(gap)[names(gap) == gap_ind[i]] <- gap_colnames[i]
+}
+
+map_latest <- function(a, b, yr = "None", value = NA) {
+  for (i in 1:length(a)) {
+    ifelse(is.na(b[i]), value, value <- b[i])
+    if (!is.na(value)) {
+      yr <- a[i]
+      return(c(yr, value))
+    }
+  }
+  return(c(yr, value))
 }
